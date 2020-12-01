52891e941108755c92b0a510c11c506971cffce0
Yan <yankaiquecosta.yk@gmail.com>
2020-12-01 08:46:42 -0300

    chore: update cart total and total itens in cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,23 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const totalValueInCart = product.quantity * product.price;
 
-    return formatValue(0);
+      return accumulator + totalValueInCart;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const totalValueInCart = product.quantity;
 
-    return 0;
+      return accumulator + totalValueInCart;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
